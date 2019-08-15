c7bf6b696a403a63b8fb5e1ecc6dde2497a9d80d
xinge <646344393@qq.com>
2019-08-15 15:48:48 +0800

    上机练习二增、删、改

--- src/cn/qwx/dao/Bill/BillMapper.java
+++ src/cn/qwx/dao/Bill/BillMapper.java
@@ -1,6 +1,8 @@
 package cn.qwx.dao.Bill;
 
 import cn.qwx.pojo.Bill;
+import cn.qwx.pojo.Provider;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,4 +13,24 @@ public interface BillMapper {
      * @return
      */
     public List<Bill> getBillByName(Bill bill);
+
+    /**
+     * 新增供应商
+     * @return
+     */
+    public int add(Provider provider);
+
+    /**
+     * 修改供应商
+     * @param provider
+     * @return
+     */
+    public int modify(Provider provider);
+
+    /**
+     * 根据id删除供应商信息
+     * @param Id
+     * @return
+     */
+    public int deleteById(@Param("id") Integer id);
 }
--- test/cn/qwx/test/BillMapperTest.java
+++ test/cn/qwx/test/BillMapperTest.java
@@ -2,11 +2,13 @@ package cn.qwx.test;
 
 import cn.qwx.dao.Bill.BillMapper;
 import cn.qwx.pojo.Bill;
+import cn.qwx.pojo.Provider;
 import cn.qwx.utils.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 public class BillMapperTest {
@@ -34,5 +36,78 @@ public class BillMapperTest {
                     +",账单金额："+bill1.getTotalPrice()
                     + ",是否付款:"+bill1.getIsPayment()+",创建时间："+bill1.getCreationDate());
         }
+    }
+
+    @Test
+    public void handsAdd(){
+        SqlSession sqlSession=null;
+        Provider provider=new Provider();
+        provider.setProCode("JS_GYS001");
+        provider.setProName("小灰机");
+        provider.setProDesc("长期合作伙伴，主营产品：玩具等");
+        provider.setProContact("王静玲");
+        provider.setProPhone("12345678912");
+        provider.setProAddress("北京");
+        provider.setProFax("0735-123456");
+        provider.setCreatedBy(1);
+        provider.setCreationDate(new Date());
+        int count=0;
+        try {
+            sqlSession=MyBatisUtil.createSqlSession();
+            count=sqlSession.getMapper(BillMapper.class).add(provider);
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            MyBatisUtil.closeSession(sqlSession);
+        }
+        logger.debug("test2=====>:"+count);
+    }
+
+    @Test
+    public void handsModify(){
+        SqlSession sqlSession=null;
+        Provider provider=new Provider();
+        provider.setId(16);
+        provider.setProCode("JS_GYS001");
+        provider.setProName("大灰机");
+        provider.setProDesc("长期合作伙伴，主营产品：玩具等");
+        provider.setProContact("王静玲");
+        provider.setProPhone("12345678912");
+        provider.setProAddress("上海浦东新区");
+        provider.setProFax("0735-123456");
+        provider.setModifyBy(5);
+        provider.setModifyDate(new Date());
+        int count=0;
+        try {
+            sqlSession=MyBatisUtil.createSqlSession();
+            count=sqlSession.getMapper(BillMapper.class).modify(provider);
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            MyBatisUtil.closeSession(sqlSession);
+        }
+        logger.debug("testModify=====>:"+count);
+    }
+
+    @Test
+    public void testDeleteById(){
+        SqlSession sqlSession=null;
+        Integer Id=16;
+        int count=0;
+        try {
+            sqlSession=MyBatisUtil.createSqlSession();
+            count=sqlSession.getMapper(BillMapper.class).deleteById(Id);
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            MyBatisUtil.closeSession(sqlSession);
+        }
+        logger.debug("testDelete=====>:"+count);
     }
 }
